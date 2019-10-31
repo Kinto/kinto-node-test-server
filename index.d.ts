f89f5b031a95224d3cc96239c5169ba8d3c2c97f
Dylan Staley <88163+dstaley@users.noreply.github.com>
2019-10-31 14:11:30 -0700

    add TypeScript declaration file

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+export interface KintoServerOptions {
+    maxAttempts?: number;
+    pservePath?: string;
+    kintoConfigPath?: string;
+}
+export declare class KintoServer {
+    private url;
+    private process;
+    private logs;
+    http_api_version: string | null;
+    private options;
+    constructor(url: string, options?: KintoServerOptions);
+    private _retryRequest;
+    start(env: Record<string, string>): Promise<void>;
+    ping(): Promise<void>;
+    flush(): Promise<void>;
+    stop(): Promise<void>;
+    killAll(): Promise<void>;
+}
